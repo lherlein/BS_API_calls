c597b84c9e0d36ef8aa78d89725c54d403511e37
Luca Herlein <luca@herlein.com>
2022-06-09 14:31:32 -0600

    comments

--- reboot.sh
+++ reboot.sh
@@ -1,14 +1,14 @@
-date_time=$(date)
+date_time=$(date) #pull date/time
 msg="pre reboot logs "
 
-touch output_log.txt
+touch output_log.txt #create output file
 
-echo "${msg} ${date_time}" > output_log.txt
+echo "${msg} ${date_time}" > output_log.txt #put message and date/time into output file
 
-curl 192.168.1.9/api//v1/logs/ -o output_log_a.txt
+curl 192.168.1.9/api//v1/logs/ -o output_log_a.txt #GET logs
 
-cat output_log_a.txt >> output_log.txt
+cat output_log_a.txt >> output_log.txt #concatenate files
 
-rm output_log_a.txt
+rm output_log_a.txt #delete temp log file
 
-curl -X PUT 192.168.1.9/api//v1/control/reboot/
+curl -X PUT 192.168.1.9/api//v1/control/reboot/ #reboot
--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #Need jq to parse json file -- sudo apt install jq
 
-value=$(curl 192.168.1.9/api//v1/control/local-dws/ | jq '.data.result.value')
+value=$(curl 192.168.1.9/api//v1/control/local-dws/ | jq '.data.result.value') 
+#GET .json object that tells if dws is on or off
+#DWS on if value = true, off if value = false
+#value is located at data: result: value
 
-echo ${value}
 
-if [ value = "true " ]
+echo ${value} #display value -debug
+
+if [ value = "true" ] #Temp - goal: if true do nothing, if false turn on
 then
 	echo 'True'
 else
